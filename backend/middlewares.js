7b477b75ccc025d151f43cd05f5e19cce3e963de
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2024-07-30 00:18:25 +0530

    step 8 done

---
+++ backend/middlewares.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken")
+const {JWT_SECRET} = require("./config")
+
+const authMiddleware = (req,res,next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({});
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Error 401"
+        })
+    }
+}
+
+module.exports = {
+    authMiddleware
+}
