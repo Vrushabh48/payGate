9a8cbb9d231802754c3eafdc6944da059be917cd
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2024-08-04 00:31:19 +0530

    Signup/Signin Components done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello There</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/signin' element={<Signin />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/send' element={<SendMoney />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
